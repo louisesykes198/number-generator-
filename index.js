129952f0964b65f447b2608d15af1d3b7bb82407
Louise Sykes <louisesykes198@yahoo.com>
2024-10-27 11:12:27 +0000

    Add corrected the error on the javascript need to add ) and also delete )

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const min = 1;
 const max = 6;
 let randomNum;
 
-myButton.onclick = function({
+myButton.onclick = function(){
     randomNum = Math.floor(Math.random() * max) + min;
     myLabel.textContent = randomNum;
-})
+}
